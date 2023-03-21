2b9a9a8d091ca1f85232213418b464c764da08d3
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-03-21 11:42:00 -0300

    add display component

--- vue/calculator/src/components/Display.vue
+++ vue/calculator/src/components/Display.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="display">0</div>
+  <div class="display">{{ value }}</div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["value"],
+};
 </script>
 
-<style></style>
+<style>
+.display {
+  grid-column: span 4;
+  background-color: #0004;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 2.1em;
+  overflow: hidden;
+}
+</style>
