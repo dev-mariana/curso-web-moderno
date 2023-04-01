985aa943b5c199237b76f6fbd728a288bbb09f51
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-04-01 16:40:29 -0300

    add graphic interface second part

--- vue/desktop-app/src/components/HomePage.vue
+++ vue/desktop-app/src/components/HomePage.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="pills">
-    <PillComponent
-      v-for="item in groupedWords"
-      :word="item.word"
-      :amount="item.amount"
-      :key="item.word"
-    />
+  <div class="home">
+    <v-form>
+      <v-file-input
+        label="Selecione as legendas"
+        prepend-icon="mdi-message-text"
+        append-icon="mdi-send"
+        outlined
+        multiple
+        v-model="files"
+        @click:append="processSubtitles"
+      >
+      </v-file-input>
+    </v-form>
+    <div class="pills">
+      <PillComponent
+        v-for="item in groupedWords"
+        :word="item.word"
+        :amount="item.amount"
+        :key="item.word"
+      />
+    </div>
   </div>
 </template>
 
@@ -18,6 +32,7 @@ export default {
   },
   data: function () {
     return {
+      files: [],
       groupedWords: [
         { word: "i", amount: 547 },
         { word: "you", amount: 478 },
@@ -25,11 +40,22 @@ export default {
       ],
     };
   },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    },
+  },
 };
 </script>
 
 <style scoped>
+.home {
+  margin: 20px;
+}
+
 .pills {
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
