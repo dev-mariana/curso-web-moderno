c85adafc685d47cb58235d30c4fe08c32f6e338f
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-04-14 22:23:27 -0300

    add processing files III

--- vue/desktop-app/src/backend/index.js
+++ vue/desktop-app/src/backend/index.js
@@ -1,17 +1,13 @@
 const { ipcMain } = require("electron");
 const pathToRows = require("./pathsToRows");
 const prepareData = require("./prepareData");
+const groupWords = require("./groupWords");
 
 ipcMain.on("process-subtitles", (event, paths) => {
-  console.log(paths);
   pathToRows(paths)
-    .then((rows) => console.log(rows))
+    .then((rows) => prepareData(rows))
     .then((preparedData) => groupWords(preparedData))
-    .then(() => {
-      event.reply("process-subtitles", [
-        { word: "i", amount: 547 },
-        { word: "you", amount: 478 },
-        { word: "it", amount: 10 },
-      ]);
+    .then((groupedWords) => {
+      event.reply("process-subtitles", groupedWords);
     });
 });
